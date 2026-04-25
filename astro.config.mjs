fd2e79341ad595514081746d045a31c5ff862e57
Gabriel Cozma <me@gxbs.dev>
2026-04-25 23:48:40 +0300

    fix: build

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ EventEmitter.defaultMaxListeners = 30;
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   adapter: node({ mode: 'standalone' }),
   integrations: [svelte()],
 
